251319edad248cf9bbb9b8e4d88a3e9771dabd73
Jake Allan <jjemail1995@gmail.com>
2021-12-03 11:17:03 +0000

    day03 part02

--- src/main/scala/problems/Day03.scala
+++ src/main/scala/problems/Day03.scala
@@ -7,21 +7,12 @@ import scala.collection.mutable.ListBuffer
 
 object Day03 {
 
-    def determineRates(fileLocation: String) : (Int, Int) = {
+    def determineRatesByBitSummation(fileLocation: String) : (Int, Int) = {
         val lines = FileUtils.readFile(fileLocation)
 
-        val bitLists = lines
-            .map(x => x.split(""))
-            .map(x => {
-                x.map(y => y.toInt)
-            })
-            .map(x => x.toList)
+        val bitLists = resolveBitLists(lines)
 
-        val bitSum : List[Int] = bitLists
-            .reduce((x, y) => {
-                x.zip(y).map((a, b) => a + b)
-            }
-        )
+        val bitSum : List[Int] = calculateBitSum(bitLists)
 
         var epsilonRateBuffer = ListBuffer[String]()
         var gammaRateBuffer = ListBuffer[String]()
@@ -44,4 +35,69 @@ object Day03 {
         (Integer.parseInt(epsilonRate, 2), Integer.parseInt(gammaRate, 2))
     }
 
+    def resolveBitLists(lines: List[String]) : List[List[Int]] = {
+        lines
+            .map(x => x.split(""))
+            .map(x => {
+                x.map(y => y.toInt)
+            })
+            .map(x => x.toList)
+    }
+
+    def calculateBitSum(bitLists: List[List[Int]]) : List[Int] = {
+        bitLists
+            .reduce((x, y) => {
+                x.zip(y).map((a, b) => a + b)
+            })
+    }
+
+    def determineRatesByBitFrequency(fileLocation: String) : (Int, Int) = {
+        val lines = FileUtils.readFile(fileLocation)
+
+        val bitLists = resolveBitLists(lines)
+
+        val oxygenGeneratorRating = determineRating(bitLists, oxygenRatingComparer)
+            .map(x => x.toString)
+            .reduce((x, y) => x.concat(y))
+        val carbonDioxideGeneratorRating = determineRating(bitLists, carbonDioxideRatingComparer)
+            .map(x => x.toString)
+            .reduce((x, y) => x.concat(y))
+
+        (Integer.parseInt(oxygenGeneratorRating, 2), Integer.parseInt(carbonDioxideGeneratorRating, 2))
+    }
+
+    def oxygenRatingComparer(onesCount: Int, zerosCount: Int) : Boolean = {
+        onesCount >= zerosCount
+    }
+
+    def carbonDioxideRatingComparer(onesCount: Int, zerosCount: Int) : Boolean = {
+        onesCount < zerosCount
+    }
+
+    def determineRating(bitLists: List[List[Int]], comparer: (Int, Int) => Boolean) : List[Int] = {
+        var bitIdx = 0
+
+        var filteredLists = bitLists
+
+        while (filteredLists.length != 1) {
+            val onesCount = calculateBitSum(filteredLists)
+            val onesCountAtPosition = onesCount(bitIdx)
+            val zerosCountAtPosition = filteredLists.length - onesCountAtPosition
+
+            if (comparer(onesCountAtPosition, zerosCountAtPosition)) {
+                filteredLists = filteredLists.filter(
+                    x => x(bitIdx) == 1
+                )
+
+            } else {
+                filteredLists = filteredLists.filter(
+                    x => x(bitIdx) == 0
+                )
+            }
+
+            bitIdx += 1
+        }
+
+        filteredLists.head
+    }
 }
--- src/test/scala/problems/Day03Test.scala
+++ src/test/scala/problems/Day03Test.scala
@@ -6,14 +6,26 @@ import org.scalatest.funsuite.AnyFunSuite
 class Day03Test extends AnyFunSuite {
 
     test("part one example input") {
-        val (gammaRate, epsilonRate) = Day03.determineRates("day03/example.txt")
+        val (gammaRate, epsilonRate) = Day03.determineRatesByBitSummation("day03/example.txt")
 
         assert(gammaRate * epsilonRate == 198)
     }
 
     test("part one puzzle input") {
-        val (gammaRate, epsilonRate) = Day03.determineRates("day03/puzzleInput.txt")
+        val (gammaRate, epsilonRate) = Day03.determineRatesByBitSummation("day03/puzzleInput.txt")
 
         assert(gammaRate * epsilonRate == 3633500)
+    }
+
+    test("part two example input") {
+        val (gammaRate, epsilonRate) = Day03.determineRatesByBitFrequency("day03/example.txt")
+
+        assert(gammaRate * epsilonRate == 230)
+    }
+
+    test("part two puzzle input") {
+        val (gammaRate, epsilonRate) = Day03.determineRatesByBitFrequency("day03/puzzleInput.txt")
+
+        assert(gammaRate * epsilonRate == 4550283)
     }
 }
